6c7ede118cebc8ceecdb8c109bd4e06a8966e4c1
ben-jhkim <kjh8@torder.com>
2024-03-18 14:20:12 +0900

    feature: LocationCode 지역코드 추가
    - SGIS 통계지리정보서비스 시도 코드표
    
    [TCD-4297]

--- lib/domain/entity/location_code.dart
+++ lib/domain/entity/location_code.dart
@@ -1,24 +1,25 @@
 enum LocationCode {
-  daegu("대구"),
-  chungnam("충남"),
-  incheon("인천"),
-  daejeon("대전"),
-  gyeongbuk("경북"),
-  sejong("세종"),
-  gwangju("광주"),
-  jeonbuk("전북"),
-  gangwon("강원"),
-  ulsan("울산"),
-  jeonnam("전남"),
-  seoul("서울"),
-  busan("부산"),
-  jeju("제주"),
-  chungbuk("충북"),
-  gyeongnam("경남"),
-  gyeonggi("경기"),
-  undefined("");
+  daegu(22, "대구"),
+  chungnam(34, "충남"),
+  incheon(23, "인천"),
+  daejeon(25, "대전"),
+  gyeongbuk(37, "경북"),
+  sejong(29, "세종"),
+  gwangju(24, "광주"),
+  jeonbuk(35, "전북"),
+  gangwon(32, "강원"),
+  ulsan(26, "울산"),
+  jeonnam(36, "전남"),
+  seoul(11, "서울"),
+  busan(21, "부산"),
+  jeju(39, "제주"),
+  chungbuk(33, "충북"),
+  gyeongnam(38, "경남"),
+  gyeonggi(31, "경기"),
+  undefined(-1, "");
 
-  const LocationCode(this.locationName);
+  const LocationCode(this.code, this.locationName);
 
+  final int code;
   final String locationName;
 }
