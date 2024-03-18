438d63865fed12aa9afcc22311ac322ea745d509
ben-jhkim <kjh8@torder.com>
2024-03-18 11:23:36 +0900

    feature: 먼지 정보에 시간 정보 추가
    [TCD-4297]

--- lib/domain/entity/dust_info.dart
+++ lib/domain/entity/dust_info.dart
@@ -1,6 +1,11 @@
 class DustInfo {
   final int rawValue;
+  final DateTime dateTime;
   final String status;
 
-  DustInfo({required this.rawValue, required this.status});
+  DustInfo({
+    required this.rawValue,
+    required this.dateTime,
+    required this.status,
+  });
 }
