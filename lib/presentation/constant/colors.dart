476aaad7de1ebfe39d5c44bc85f7b791c66d237b
ben-jhkim <kjh8@torder.com>
2024-03-18 17:38:58 +0900

    feature: 기본 상수값 추가
    [TCD-4273]

---
+++ lib/presentation/constant/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorResource {
+  ColorResource._();
+
+  static final PRIMARY_COLOR = Colors.blue[800];
+  static final BACKGROUND_COLOR = Colors.blue[400];
+}
---
+++ lib/presentation/constant/strings.dart
@@ -0,0 +1,16 @@
+class Strings {
+  Strings._();
+
+  static const FINE_DUST_TITLE = "미세먼지 정보";
+  static const LOCATION_FINE_DUST_TITLE = "지역별 미세먼지 정보";
+  static const LOCATION_FINE_INFO_ERROR = "지역 별 미세먼지 정보를 가져오지 못했습니다.";
+  static const LOCATION_FINE_INFO_IS_EMPTY = "지역 별 미세먼지 정보가 없습니다.";
+  static const FINE_DUST = "미세먼지";
+  static const ULTRA_FINE_DUST = "초미세먼지";
+  static const OZONE = "오존";
+
+  static const MICRO_PER_CUBIC_METER_UNIT = "㎍/㎥";
+  static const PARTS_PER_MILLION_UNIT = "ppm";
+
+
+}
