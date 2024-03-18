e2b4f90042c82570d9e958a6cd12d96de08f5948
kite-torder <161293034+kite-torder@users.noreply.github.com>
2024-03-18 15:19:47 +0900

    Update fine_dust_repository.dart

--- lib/domain/repository/fine_dust_repository.dart
+++ lib/domain/repository/fine_dust_repository.dart
@@ -2,8 +2,8 @@ import 'package:fine_dust/domain/entity/location_Info.dart';
 import 'package:fine_dust/domain/entity/location_total_info.dart';
 
 abstract interface class FineDustRepository {
-  Future<List<DustInfo>> getFineDustInfoList();
-  Future<List<DustInfo>> getUltraFineDustInfoList();
-  Future<List<DustInfo>> getOzoneInfoList();
-  Future<LocationInfo> getLocalAirInfoUsecase({required int locationId});
+  Future<List<LocationInfo>> getFineDustInfoList();
+  Future<List<LocationInfo>> getUltraFineDustInfoList();
+  Future<List<LocationInfo>> getOzoneInfoList();
+  Future<LocationTotalInfo> getLocalAirInfo({required int locationId});
 }
