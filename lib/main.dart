7138f1c11e4c5a4b285045446dea1b67deb45a01
ben-jhkim <kjh8@torder.com>
2024-03-19 16:32:05 +0900

    feature: BookmarkRepository 주입
    [TCD-4326]

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:fine_dust/data/remote/repository/bookmark_repository_impl.dart';
 import 'package:fine_dust/data/remote/repository/fine_dust_repository_impl.dart';
 import 'package:fine_dust/domain/repository/fine_dust_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'data/remote/datasource/bookmark_firestore.dart';
 import 'data/remote/datasource/dust_info_api.dart';
+import 'domain/repository/bookmark_repository.dart';
 import 'presentation/constant/theme.dart';
 import 'presentation/screen/location_fine_dust/view/location_fine_dust_list_screen.dart';
 
@@ -14,10 +17,17 @@ void main() async {
   final FineDustRepository repository =
       FineDustRepositoryImpl(api: DustInfoApi());
 
+
+  final bookmarkFireStore = BookmarkFirestore();
+  await bookmarkFireStore.init();
+
+  final BookmarkRepository bookmarkRepository = BookmarkRepositoryImpl(firestore: bookmarkFireStore);
+
   runApp(
     MultiRepositoryProvider(
       providers: [
         RepositoryProvider<FineDustRepository>(create: (context) => repository),
+        RepositoryProvider<BookmarkRepository>(create: (context) => bookmarkRepository),
       ],
       child: MaterialApp(
         home: const LocationFineDustListScreen(),
