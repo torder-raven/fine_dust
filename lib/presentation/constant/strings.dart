0d44cbaf8bc81bc0bcf64cea29e698b658d8a3f2
ben-jhkim <kjh8@torder.com>
2024-03-19 11:46:24 +0900

    feature: 지역별 문구 수정
    [TCD-4273]

--- lib/presentation/constant/strings.dart
+++ lib/presentation/constant/strings.dart
@@ -3,8 +3,8 @@ class Strings {
 
   static const FINE_DUST_TITLE = "미세먼지 정보";
   static const LOCATION_FINE_DUST_TITLE = "지역별 미세먼지 정보";
-  static const LOCATION_FINE_INFO_ERROR = "지역 별 미세먼지 정보를 가져오지 못했습니다.";
-  static const LOCATION_FINE_INFO_IS_EMPTY = "지역 별 미세먼지 정보가 없습니다.";
+  static const LOCATION_FINE_INFO_ERROR = "지역별 미세먼지 정보를 가져오지 못했습니다.";
+  static const LOCATION_FINE_INFO_IS_EMPTY = "지역별 미세먼지 정보가 없습니다.";
   static const FINE_DUST = "미세먼지";
   static const ULTRA_FINE_DUST = "초미세먼지";
   static const OZONE = "오존";
