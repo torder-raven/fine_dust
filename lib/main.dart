75604b63aacf1bdbe8dede8155944442be9167a8
ben-jhkim <kjh8@torder.com>
2024-03-19 09:05:15 +0900

    feature: LocationFineDustListScreen 처음 화면 으로 변경
    - HomeScreen 제거
    - RepositoryProvider 적용
    
    [TCD-4273]

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
-import 'package:fine_dust/presentation/screen/home/home_screen.dart';
+import 'package:fine_dust/data/remote/repository/fine_dust_repository_impl.dart';
+import 'package:fine_dust/domain/repository/fine_dust_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'data/remote/datasource/dust_info_api.dart';
+import 'presentation/constant/theme.dart';
+import 'presentation/screen/location_fine_dust/view/location_fine_dust_list_screen.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
 
-  runApp(MaterialApp(
-    home: HomeScreen(),
-  ));
+  final FineDustRepository repository =
+      FineDustRepositoryImpl(api: DustInfoApi());
+
+  runApp(
+    MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<FineDustRepository>(create: (context) => repository),
+      ],
+      child: MaterialApp(
+        home: const LocationFineDustListScreen(),
+        theme: findDustTheme,
+      ),
+    ),
+  );
 }
