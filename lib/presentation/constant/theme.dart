a36cd952993b4b18ed2a7f6f6d43036fa7180c8f
ben-jhkim <kjh8@torder.com>
2024-03-19 10:11:53 +0900

    feature: reformat ThemeData
    [TCD-4273]

--- lib/presentation/constant/theme.dart
+++ lib/presentation/constant/theme.dart
@@ -5,11 +5,12 @@ import 'colors.dart';
 final findDustTheme = ThemeData(
   primaryColor: ColorResource.PRIMARY_COLOR,
   appBarTheme: AppBarTheme(
-      color: ColorResource.PRIMARY_COLOR,
-      iconTheme: const IconThemeData(color: Colors.white),
-      titleTextStyle: const TextStyle(
-        fontSize: 28.0,
-        fontWeight: FontWeight.w800,
-        color: Colors.white,
-      )),
+    color: ColorResource.PRIMARY_COLOR,
+    iconTheme: const IconThemeData(color: Colors.white),
+    titleTextStyle: const TextStyle(
+      fontSize: 28.0,
+      fontWeight: FontWeight.w800,
+      color: Colors.white,
+    ),
+  ),
 );
