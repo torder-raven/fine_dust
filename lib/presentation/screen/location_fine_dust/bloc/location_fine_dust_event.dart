5131be52a3f04f472ece6cf4d179891731628e4a
ben-jhkim <kjh8@torder.com>
2024-03-19 16:28:49 +0900

    feature: Bloc에 북마크 이벤트 추가
    - 북마크 추가
    - 북마크 삭제
    - 북마크 조회
    
    [TCD-4326]

--- lib/presentation/screen/location_fine_dust/bloc/location_fine_dust_event.dart
+++ lib/presentation/screen/location_fine_dust/bloc/location_fine_dust_event.dart
@@ -3,4 +3,7 @@ part of 'location_fine_dust_bloc.dart';
 @freezed
 class LocationFineDustEvent with _$LocationFineDustEvent {
   const factory LocationFineDustEvent.fetch() = _Fetch;
+  const factory LocationFineDustEvent.refreshBookmarkList(List<int> bookmarkList) = _RefreshBookmarkList;
+  const factory LocationFineDustEvent.bookmark(LocationCode locationCode) = _Bookmark;
+  const factory LocationFineDustEvent.deleteBookmark(LocationCode locationCode) = _DeleteBookmark;
 }
