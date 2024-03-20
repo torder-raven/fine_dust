8a385a8aee371302375608ea0a7cedd4b038a2df
ben-jhkim <kjh8@torder.com>
2024-03-20 14:52:59 +0900

    refactor: ColorResource const 색상으로 변경
    [TCD-4360]

--- lib/presentation/constant/colors.dart
+++ lib/presentation/constant/colors.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class ColorResource {
   ColorResource._();
 
-  static final PRIMARY_COLOR = Colors.blue[800];
-  static final BACKGROUND_COLOR = Colors.blue[400];
+  static const PRIMARY_COLOR = Color(0xFF1565C0);
+  static const BACKGROUND_COLOR = Color(0xFF42A5F5);
 }
