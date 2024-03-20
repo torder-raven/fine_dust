0b158fdcefed3ed04ec6f53853ba0f25feb7e1a2
ben-jhkim <kjh8@torder.com>
2024-03-20 19:23:21 +0900

    refactor: Strings 미사용 리소스 제거
    [TCD-4360]

--- lib/presentation/constant/strings.dart
+++ lib/presentation/constant/strings.dart
@@ -2,9 +2,7 @@ class Strings {
   Strings._();
 
   static const FINE_DUST_TITLE = "미세먼지 정보";
-  static const LOCATION_FINE_DUST_TITLE = "지역별 미세먼지 정보";
-  static const LOCATION_FINE_INFO_ERROR = "지역별 미세먼지 정보를 가져오지 못했습니다.";
-  static const LOCATION_FINE_INFO_IS_EMPTY = "지역별 미세먼지 정보가 없습니다.";
+  static const LOCATION_FINE_INFO_ERROR = "미세먼지 정보를 가져오지 못했습니다.";
   static const FINE_DUST = "미세먼지";
   static const ULTRA_FINE_DUST = "초미세먼지";
   static const OZONE = "오존";
